9e7f23c8915a52b5f219891932948a4a8d4785ea
chinatip <chinatip.v@ku.th>
2017-06-13 11:25:57 +0700

    Add edit value to RadarChart

--- src/RadarChart.js
+++ src/RadarChart.js
@@ -1,29 +1,40 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Chart from "react-chartjs"
 import ScoreItem from './ScoreItem'
 import {map} from 'lodash'
-import { RadarChartWrapper  } from './scoreboard-styles'
+import {RadarChartWrapper} from './scoreboard-styles'
 
 const RadarChart = Chart.Radar;
 
-class ScoreBoard extends Component {
-  // constructor(props) {
-  //   super();
-  //   this.state = {
-  //     data: props.data,
-  //   }
-  // }
+class RedarChart extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      data: props.data || []
+    };
+  }
 
-  // componentWillUpdate(nextProps, nextState) {
-  //   if (nextProps.data !== this.props.data) {
-  //     this.setState({ data: this.props.data })
-  //   }
-  // }
+  updateValue(key, e) {
+    console.log("click Key", key)
+    console.log("e", e.target.value)
+    let newData = []
+    for(let i=0; i<this.state.data.length; i++) {
+      if(i == key)
+        newData.push({
+          name: this.state.data[i].name,
+          value: e.target.value
+        })
+      else
+      newData.push(this.state.data[i])
+    }
+    console.log("newData", newData)
+    this.setState({data: newData})
+  }
 
   render() {
     let labels = [];
     let data = [];
-    (this.props.data).forEach(function(item){
+    (this.state.data).forEach(function (item) {
       labels.push(item.name)
       data.push(item.value)
     })
@@ -32,35 +43,44 @@ class ScoreBoard extends Component {
     console.log("data", data)
 
     let chartData = {
-        labels: labels,
-        datasets: [{
-            data: data,
-            backgroundColor: [
-                'rgba(0, 0, 0, 0.1)',
-                'rgba(0, 0, 0, 0.1)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)'
-            ],
-            borderWidth: 1
-        }]};
-    let chartOptions = {options: {
-        
-        
-    }}
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: [
+            'rgba(0, 0, 0, 0.1)', 'rgba(0, 0, 0, 0.1)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    };
+    let chartOptions = {
+      options: {}
+    }
     return (
       <RadarChartWrapper>
-        {
-            map(this.props.data, (value, key) => {
-            return (
-              <ScoreItem key={ key } name={value.name} value={value.value} data={this.props.data}></ScoreItem>
-            );
-          })}
-        <RadarChart redraw data={chartData} options={chartOptions} width="600" height="250"/>
+        {this.state.data.map(function(value, key) {
+          return (
+            <ScoreItem
+              id={key}
+              name={value.name}
+              value={value.value}
+              data={this.state.data}
+              updateValue={(key, e) => this.updateValue(key, e)}></ScoreItem>
+          )
+        }.bind(this))}
+        <RadarChart
+          redraw
+          data={chartData}
+          options={chartOptions}
+          width="600"
+          height="250"/>
       </RadarChartWrapper>
     );
   }
 }
 
-export default ScoreBoard
+export default RedarChart
--- src/ScoreItem.js
+++ src/ScoreItem.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
-import { ScoreItemWrapper, InputScore, InputCriteria, DeleteButton } from './scoreboard-styles'
+import React, {Component} from 'react'
+import {ScoreItemWrapper, InputScore, InputCriteria, DeleteButton} from './scoreboard-styles'
 
 class ScoreItem extends Component {
   constructor(props) {
-      super();
-  }
-
-  updateValue = (event) => {
-    // this.props.data[this.props.key].value = event.target.value;
-    console.log("event", event.target.value)
-    console.log("key", this.props.key)
+    super();
   }
 
   render() {
-      return (
-        <ScoreItemWrapper>
-            <InputCriteria value={this.props.name}/>
-            <InputScore type="number" name="score" min="1" max="20" defaultValue={this.props.value} onChange={this.updateValue}></InputScore>
-            <DeleteButton>X</DeleteButton>
-        </ScoreItemWrapper>
-      );
+    return (
+      <ScoreItemWrapper>
+        <InputCriteria value={this.props.name}/>
+        <InputScore
+          type="number"
+          name="score"
+          min="1"
+          max="20"
+          defaultValue={this.props.value}
+          onChange={(e) => this.props.updateValue(this.props.id, e)}></InputScore>
+        <DeleteButton>X</DeleteButton>
+      </ScoreItemWrapper>
+    );
   }
 }
 
