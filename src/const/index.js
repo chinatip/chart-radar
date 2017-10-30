5a99b040580ba66b4db430c763251fa399b5f9cc
chinatip <chinatip.v@ku.th>
2017-10-31 04:16:02 +0700

    Update CreateEditController

---
+++ src/const/index.js
@@ -0,0 +1,17 @@
+export const newData = {
+  "fullname": "New User",
+  "position": "unknown",
+  "stats": {
+    "0": { "label": "label-1", "value": 8 },
+    "1": { "label": "label-2", "value": 8 },
+    "2": { "label": "label-3", "value": 8 },
+    "3": { "label": "label-4", "value": 8 },
+    "4": { "label": "label-5", "value": 8 }
+  },
+  "notes": {
+    "0": {
+      "title": "Title",
+      "text" : "Text"
+    }
+  }
+};
