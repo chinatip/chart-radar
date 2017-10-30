7a571aefc3d4a0b1ef8584a3d1ee95a3d510a421
chinatip <chinatip.v@ku.th>
2017-10-31 03:49:24 +0700

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,11 @@ class routes extends Component {
   render() {
     return (
       <div className="routes">
-        <GlobalStyles/>
+        <GlobalStyles />
         <Switch>
-          <Route exact path="/" component={ Home }/>
-          <Route exact path="/manage" component={ ManageUser }/>
-          <Route exact path="/category/:category" component={ Category }/>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/manage" component={ ManageUser } />
+          <Route exact path="/category/:category" component={ Category } />
           <Route path="/edit/:userId" component={ EditUser } />
         </Switch>
       </div>
